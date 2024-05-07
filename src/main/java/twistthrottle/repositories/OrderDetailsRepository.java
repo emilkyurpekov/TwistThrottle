1e91acaaeb0d4f895407dc2f152df8295c3e0485
Emil <emil.kiurpekov4@gmail.com>
2024-05-07 13:27:23 +0300

    updated repositories

--- src/main/java/twistthrottle/repositories/OrderDetailsRepository.java
+++ src/main/java/twistthrottle/repositories/OrderDetailsRepository.java
@@ -1,4 +1,7 @@
 package twistthrottle.repositories;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface OrderDetailsRepository {
 }
--- src/main/java/twistthrottle/repositories/OrderRepository.java
+++ src/main/java/twistthrottle/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package twistthrottle.repositories;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository {
 }
