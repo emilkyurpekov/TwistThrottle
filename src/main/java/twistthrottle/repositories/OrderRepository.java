82fab04e98ca68daa96b85a5d3395adbff8e1cba
Emil <emil.kiurpekov4@gmail.com>
2024-05-08 18:00:51 +0300

    updated repositories

--- src/main/java/twistthrottle/repositories/OrderRepository.java
+++ src/main/java/twistthrottle/repositories/OrderRepository.java
@@ -1,7 +1,12 @@
 package twistthrottle.repositories;
 
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import twistthrottle.models.entities.Order;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    Order findOrderById(int id);
+    Order findOrderByUserEmail(String email);
 }
