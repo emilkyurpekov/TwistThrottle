2913f3a0e7848febd838d87c3123c26242e507e2
Emil <emil.kiurpekov4@gmail.com>
2024-05-14 20:54:13 +0300

    deleted repository

--- src/main/java/twistthrottle/repositories/OrderDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package twistthrottle.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import twistthrottle.models.entities.OrderDetails;
-
-@Repository
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
-}
