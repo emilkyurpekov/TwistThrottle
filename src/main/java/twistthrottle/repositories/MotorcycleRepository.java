0d40d53bbd7207a39de7aed2de6600a5813e7275
Emil <emil.kiurpekov4@gmail.com>
2024-05-22 20:31:20 +0300

    updated repository

--- src/main/java/twistthrottle/repositories/MotorcycleRepository.java
+++ src/main/java/twistthrottle/repositories/MotorcycleRepository.java
@@ -2,6 +2,7 @@ package twistthrottle.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import twistthrottle.models.entities.Motorcycle;
+import twistthrottle.models.entities.enums.motorcycleType;
 
 import java.util.List;
 
@@ -9,7 +10,9 @@ public interface MotorcycleRepository  extends JpaRepository<Motorcycle,Long> {
     List<Motorcycle> findByMake(String make);
     List<Motorcycle> findByModel(String model);
     List<Motorcycle> findByYear(int year);
+    Motorcycle findById(int id);
+    List<Motorcycle> findAllByHorsepowerBetween(int minHP, int maxHP);
     List<Motorcycle> findByHorsepowerGreaterThan(int horsepower);
     List<Motorcycle> findByHorsepowerLessThan(int horsepower);
-    List<Motorcycle> findByCategory(String category);
+    List<Motorcycle> findAllByMotorcycleType(motorcycleType motorcycleType);
 }
