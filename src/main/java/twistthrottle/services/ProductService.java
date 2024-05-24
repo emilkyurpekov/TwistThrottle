50fb2cdb97ec8f407972ea16bbf19a4fb4f4bd86
Emil <emil.kiurpekov4@gmail.com>
2024-05-25 00:14:45 +0300

    changed ProductService

--- src/main/java/twistthrottle/services/ProductService.java
+++ src/main/java/twistthrottle/services/ProductService.java
@@ -1,22 +1,8 @@
 package twistthrottle.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import twistthrottle.models.entities.Product;
-import twistthrottle.repositories.ProductRepository;
 
-import java.util.List;
 
-@Service
-public class ProductService {
-    @Autowired
-    private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+public interface ProductService {
 
-    public List<Product> getProductsSortedByPriceDesc(){
-        return productRepository.findAllByOrderByPriceDesc();
-    }
 }
