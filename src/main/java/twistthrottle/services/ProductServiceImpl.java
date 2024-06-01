d28bcfc3a29c8281d218cec23f1438848cb1b499
Emil <emil.kiurpekov4@gmail.com>
2024-06-01 18:47:11 +0300

    Service implementation added

---
+++ src/main/java/twistthrottle/services/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package twistthrottle.services;
+
+import twistthrottle.models.entities.Product;
+
+public class ProductServiceImpl implements ProductService{
+    @Override
+    public void updateProductStock(Long productId, int newStock) {
+    }
+}
