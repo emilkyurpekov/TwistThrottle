a9f6d02b7e671cd439a266432bba37e4808ef3d2
Emil <emil.kiurpekov4@gmail.com>
2024-08-30 20:32:50 +0300

    working on Showing motorcycles

--- src/main/java/twistthrottle/controllers/MotorcycleController.java
+++ src/main/java/twistthrottle/controllers/MotorcycleController.java
@@ -1,15 +1,22 @@
 package twistthrottle.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import twistthrottle.models.entities.Motorcycle;
 import twistthrottle.models.entities.User;
 import twistthrottle.services.MotorcycleServiceImpl;
+
+import java.util.List;
+
 @Controller
 public class MotorcycleController {
     @Autowired
@@ -31,5 +38,15 @@ public class MotorcycleController {
             redirectAttributes.addFlashAttribute("errorMessage", "Failed to add motorcycle.");
             return "redirect:/profile";
         }
+
+    }
+    @GetMapping("/motorcycles")
+    public ResponseEntity<List<Motorcycle>> getUserMotorcycles(HttpSession session) {
+        User user = (User) session.getAttribute("loggedInUser");
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        List<Motorcycle> motorcycles = motorcycleService.getMotorcyclesByUser(user);
+        return ResponseEntity.ok(motorcycles);
     }
 }
