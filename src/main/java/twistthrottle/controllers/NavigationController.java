16cd673795b9e9616b5a00c7864a7cf54709c905
Emil <emil.kiurpekov4@gmail.com>
2024-10-26 01:44:44 +0300

    Started working on admin and user separation

--- src/main/java/twistthrottle/controllers/NavigationController.java
+++ src/main/java/twistthrottle/controllers/NavigationController.java
@@ -2,9 +2,7 @@ package twistthrottle.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
