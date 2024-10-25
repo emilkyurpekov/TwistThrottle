65bc5cd4fdecf7208c6af4163def027b4b3b33a8
Emil <emil.kiurpekov4@gmail.com>
2024-10-26 01:43:37 +0300

    Started working on admin and user separation

--- src/main/java/twistthrottle/controllers/UserController.java
+++ src/main/java/twistthrottle/controllers/UserController.java
@@ -1,17 +1,14 @@
 package twistthrottle.controllers;
-import jakarta.servlet.http.HttpServletRequest;
+
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import twistthrottle.dtos.UserDTO;
-import twistthrottle.models.entities.Motorcycle;
 import twistthrottle.models.entities.User;
 import twistthrottle.services.MotorcycleServiceImpl;
 import twistthrottle.services.UserServiceImpl;
