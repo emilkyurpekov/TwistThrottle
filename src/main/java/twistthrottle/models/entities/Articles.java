ecb6746d2b1378600258e05163e0d9c4d592ef1b
Emil <emil.kiurpekov4@gmail.com>
2024-11-21 21:52:43 +0200

    Added Articles table

---
+++ src/main/java/twistthrottle/models/entities/Articles.java
@@ -0,0 +1,11 @@
+package twistthrottle.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+public class Articles extends BaseEntity {
+
+
+}
