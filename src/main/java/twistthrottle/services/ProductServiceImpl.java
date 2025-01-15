0053d6302e33eff47612926ea588e66cfa3782a5
Emil <emil.kiurpekov4@gmail.com>
2025-01-15 20:17:59 +0200

    Started working on products page and implementation

--- src/main/java/twistthrottle/services/ProductServiceImpl.java
+++ src/main/java/twistthrottle/services/ProductServiceImpl.java
@@ -39,7 +39,7 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return productRepository.findAll();
     }
 
     @Override
