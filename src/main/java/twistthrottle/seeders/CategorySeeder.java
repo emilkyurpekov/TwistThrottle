be34f4b0f7990d3751291ae43db776c8e4a38b22
Emil <emil.kiurpekov4@gmail.com>
2025-01-26 20:48:10 +0200

    added seeder for the categories table that is checking if the table is empty and it populates it with the three categories and if it is not empty it does nothing

---
+++ src/main/java/twistthrottle/seeders/CategorySeeder.java
@@ -0,0 +1,46 @@
+package twistthrottle.seeders;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import twistthrottle.models.entities.Category;
+import twistthrottle.models.entities.enums.categoryType;
+import twistthrottle.repositories.CategoryRepository;
+
+@Component
+public class CategorySeeder implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategorySeeder(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (categoryRepository.count() == 0) {
+            Category exhaustSystems = new Category(
+                    "Exhaust Systems",
+                    categoryType.EXHAUST_SYSTEMS,
+                    "Components related to exhaust systems."
+            );
+            Category engineComponents = new Category(
+                    "Engine Components",
+                    categoryType.ENGINE_COMPONENTS,
+                    "Essential parts for engine systems."
+            );
+            Category accessories = new Category(
+                    "Accessories",
+                    categoryType.ACCESSORIES,
+                    "Various motorcycle accessories."
+            );
+
+            categoryRepository.save(exhaustSystems);
+            categoryRepository.save(engineComponents);
+            categoryRepository.save(accessories);
+
+            System.out.println("Categories have been seeded!");
+        } else {
+            System.out.println("Categories already exist. Skipping seeding.");
+        }
+    }
+}
