4f70c87054fc1180a530777354d74db6a526382d
Emil <emil.kiurpekov4@gmail.com>
2025-03-03 15:14:03 +0200

    working on RestAPI

--- src/main/java/twistthrottle/dtos/UserDTO.java
+++ src/main/java/twistthrottle/dtos/UserDTO.java
@@ -1,41 +1,13 @@
 package twistthrottle.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTO {
     private String firstName;
     private String lastName;
     private String username;
     private String email;
-
-    // Getters and Setters
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
