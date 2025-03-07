b3cc525ce9195cca60c85dc59b22407da9ac6cbe
Emil <emil.kiurpekov4@gmail.com>
2025-03-07 12:14:00 +0200

    Added lombok properties

--- src/main/java/twistthrottle/models/entities/Order.java
+++ src/main/java/twistthrottle/models/entities/Order.java
@@ -7,13 +7,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import twistthrottle.models.entities.enums.orderStatus;
 import twistthrottle.models.entities.enums.paymentMethod;
+
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order extends BaseEntity{
-
     @Temporal(TemporalType.DATE)
     @Column(name = "order_date", nullable = false)
     private Date orderDate;
@@ -35,74 +39,4 @@ public class Order extends BaseEntity{
     @JoinColumn(name = "user_id")
     private User user;
 
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public orderStatus getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(orderStatus orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public paymentMethod getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(paymentMethod paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getShippingAddress() {
-        return shippingAddress;
-    }
-
-    public void setShippingAddress(String shippingAddress) {
-        this.shippingAddress = shippingAddress;
-    }
-
-    public String getBillingAddress() {
-        return billingAddress;
-    }
-
-    public void setBillingAddress(String billingAddress) {
-        this.billingAddress = billingAddress;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Order() {
-    }
-
-    public Order(Date date, orderStatus orderStatus, double totalPrice, paymentMethod paymentMethod, String shippingAddress, String billingAddress, List<OrderDetails> orderDetails, User user) {
-        this.orderDate = date;
-        this.orderStatus = orderStatus;
-        this.totalPrice = totalPrice;
-        this.paymentMethod = paymentMethod;
-        this.shippingAddress = shippingAddress;
-        this.billingAddress = billingAddress;
-        this.orderDetails = orderDetails;
-        this.user = user;
-    }
 }
--- src/main/java/twistthrottle/models/entities/OrderDetails.java
+++ src/main/java/twistthrottle/models/entities/OrderDetails.java
@@ -1,53 +1,24 @@
 package twistthrottle.models.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class OrderDetails extends BaseEntity {
-
-
-
     @Column(nullable = false)
     private int quantity;
     @Column(nullable = false)
     private BigDecimal unitPrice;
-
-    public OrderDetails() {
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(BigDecimal unitPrice) {
-        this.unitPrice = unitPrice;
-    }
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
