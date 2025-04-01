5f340775bc4b1005337dc2849a695332f9ec1a58
Emil <emil.kiurpekov4@gmail.com>
2025-04-01 21:15:13 +0300

    registered users now have roles and admins can change the roles of other users

--- src/main/java/twistthrottle/controllers/AdminController.java
+++ src/main/java/twistthrottle/controllers/AdminController.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import twistthrottle.models.entities.User;
 import twistthrottle.models.entities.enums.RoleType;
 import twistthrottle.services.UserService;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -49,6 +50,33 @@ public class AdminController {
 
 
         return "admin/user-edit-roles";
+    }
+    @PostMapping("/users/{userId}/roles")
+    public String updateUserRoles(@PathVariable Long userId, @RequestParam(required = false) Set<String> roles) {
+
+        User user = userService.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with id: " + userId));
+
+        Set<RoleType> newRoles = new HashSet<>();
+        if (roles != null) {
+            newRoles = roles.stream()
+                    .map(roleName -> {
+                        try {
+                            return RoleType.valueOf(roleName);
+                        } catch (IllegalArgumentException e) {
+                            System.err.println("Warning: Submitted invalid role name '" + roleName + "' for user ID " + userId);
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toSet());
+        }
+
+
+        user.setRoles(newRoles);
+        userService.saveUserWithoutEncoding(user);
+
+        return "redirect:/admin/users";
     }
 
 }
